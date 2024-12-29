650b2e006ea7294e809070538e941e371f54083f
Mohammad Adnan <65945728+adnandotdev@users.noreply.github.com>
2024-12-29 19:46:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.getElementById('change_button').addEventListener('click', function() {
+	const blockId = document.getElementById('block_id').value
+	const color = document.getElementById('colour_id').value
+	const selectedBlock = document.getElementById(blockId)
+	selectedBlock.style.backgroundColor = color
+})
+document.getElementById('Reset').addEventListener('click', function() {
+	const gridItems = document.getElementsByClassName('grid-item')
+	for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].style.backgroundColor = 'transparent'
+    }
+	document.getElementById('block_id').value = ''
+	document.getElementById('colour_id').value = ''
+})
